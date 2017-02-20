1bf6d8e85e38165be7fd687973b9c683c962c905
zacharyrote <zachary.rote@gmail.com>
2017-02-20 08:16:45 -0500

    hand unbound proxy

--- index.js
+++ index.js
@@ -11,42 +11,47 @@ class RealizationCheckMiddleware {
     return (req, res, next) => {
       let proxy = this.app.proxy;
       console.log("Checking dependencies");
-      proxy.table().then((cache) => {
-        // Where do I get dependent service types?
-        let realized = [];
-        let fullyRealized = false;
-        let missingType = null;
+      if(proxy) {
+        proxy.table().then((cache) => {
+          // Where do I get dependent service types?
+          let realized = [];
+          let fullyRealized = false;
+          let missingType = null;
 
-        for(let d in this.app.dependencies) {
-          let typeToCheck = this.app.dependencies[d];
-          console.log(`Checking for type ${typeToCheck}`);
-          let realizedDep = _.find(cache, {type:typeToCheck, status: 'Online'});
-          if(realizedDep) {
-            console.log(realizedDep.type);
-            realized.push(realizedDep.type);
-          } else {
-            console.log(`Missing type ${this.app.dependencies[d]}`);
-            missingType = this.app.dependencies[d];
-            break;
+          for(let d in this.app.dependencies) {
+            let typeToCheck = this.app.dependencies[d];
+            console.log(`Checking for type ${typeToCheck}`);
+            let realizedDep = _.find(cache, {type:typeToCheck, status: 'Online'});
+            if(realizedDep) {
+              console.log(realizedDep.type);
+              realized.push(realizedDep.type);
+            } else {
+              console.log(`Missing type ${this.app.dependencies[d]}`);
+              missingType = this.app.dependencies[d];
+              break;
+            }
           }
-        }
 
-        if(_.isEmpty(_.xor(this.app.dependencies, realized))) {
-          fullyRealized = true;
-        }
+          if(_.isEmpty(_.xor(this.app.dependencies, realized))) {
+            fullyRealized = true;
+          }
 
-        if(fullyRealized) {
-          next();
-        } else {
-          res.status(HttpStatus.SERVICE_UNAVAILABLE).send({
-            errorMessage: `Missing service dependency ${missingType}`
+          if(fullyRealized) {
+            next();
+          } else {
+            res.status(HttpStatus.SERVICE_UNAVAILABLE).send({
+              errorMessage: `Missing service dependency ${missingType}`
+            });
+          }
+        }).catch((err) => {
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+            errorMessage: err.message
           });
-        }
-      }).catch((err) => {
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
-          errorMessage: err.message
         });
-      });
+      } else {
+        // No bound proxy.
+        res.status(HttpStatus.OK).send([]);
+      }
     }
   }
 
